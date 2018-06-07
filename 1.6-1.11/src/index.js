01c965946e84a1428a37718b6a542eb8b7f80046
Jonne Kanerva <jonne.kanerva@siili.com>
2018-06-07 11:55:47 +0300

    Tehtävä 1.8

--- 1.6-1.11/src/index.js
+++ 1.6-1.11/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 
 const Otsikko = props => <h2>{props.teksti}</h2>
 
-const PalauteNappi = props => {
+const Button = props => {
   return (
-    <button>{props.teksti}</button>
+    <button onClick={props.onClick}>{props.teksti}</button>
   )
 }
 
@@ -21,38 +21,49 @@ const PalauteLista = props => {
 
 const Palaute = props => <p>{props.teksti} {props.arvo}</p>
 
-const Statistiikka = props => {
+const Statistics = props => {
+
+  const palautteita = Object.values(props.palautteet).reduce( (yht, palaute) => yht + palaute, 0)
+
+  if(palautteita === 0) {
+    return (
+      <p>ei yhtään palautetta annettu</p>
+    )
+  }
+
   return (
     <div>
-      <Keskiarvo palautteet={props.palautteet} />
-      <Positiivisia palautteet={props.palautteet} />
+      <Statistic statistic="keskiarvo" palautteet={props.palautteet} />
+      <Statistic statistic="positiivisia" palautteet={props.palautteet} />
     </div>
   )
 }
 
-const Keskiarvo = props => {
+const Statistic = props => {
 
-  const palautteita = Object.values(props.palautteet).reduce( (yht, palaute) => yht + palaute, 0)
-  const hyvatPisteet = props.palautteet.hyva * 1
-  const neutraalitPisteet = props.palautteet.neutraali * 0 // heh
-  const huonotPisteet = props.palautteet.huono * -1
+  if(props.statistic === "keskiarvo") {
+    const palautteita = Object.values(props.palautteet).reduce( (yht, palaute) => yht + palaute, 0)
+    const hyvatPisteet = props.palautteet.hyva * 1
+    const neutraalitPisteet = props.palautteet.neutraali * 0 // heh
+    const huonotPisteet = props.palautteet.huono * -1
 
-  const summa = hyvatPisteet + neutraalitPisteet + huonotPisteet
+    const summa = hyvatPisteet + neutraalitPisteet + huonotPisteet
 
-  const keskiarvo = summa / palautteita
+    const keskiarvo = summa / palautteita
 
-  return (
-    <p>keskiarvo {keskiarvo.toFixed(1)}</p>
-  )
-}
+    return (
+      <p>keskiarvo {keskiarvo.toFixed(1)}</p>
+    )
+  }
 
-const Positiivisia = props => {
-  const palautteita = Object.values(props.palautteet).reduce( (yht, palaute) => yht + palaute, 0)
-  const positiivisia = props.palautteet.hyva / palautteita * 100
+  if(props.statistic === "positiivisia") {
+    const palautteita = Object.values(props.palautteet).reduce( (yht, palaute) => yht + palaute, 0)
+    const positiivisia = props.palautteet.hyva / palautteita * 100
 
-  return (
-    <p>positiivisia {positiivisia.toFixed(1)} %</p>
-  )
+    return (
+      <p>positiivisia {positiivisia.toFixed(1)} %</p>
+    )
+  }
 }
 
 class App extends React.Component {
@@ -67,6 +78,7 @@ class App extends React.Component {
 
   lisaaPalaute = (palaute) => {
     return () => {
+      console.log("moi")
       this.setState({
         [palaute]: this.state[palaute] + 1
       })
@@ -74,15 +86,16 @@ class App extends React.Component {
   }
 
   render() {
+
     return (
       <div>
         <Otsikko teksti="anna palautetta" />
-        <button onClick={this.lisaaPalaute("hyva")}>hyvä</button>
-        <button onClick={this.lisaaPalaute("neutraali")}>neutraali</button>
-        <button onClick={this.lisaaPalaute("huono")}>huono</button>
+        <Button teksti="hyva" onClick={this.lisaaPalaute("hyva")} />
+        <Button teksti="neutraali" onClick={this.lisaaPalaute("neutraali")} />
+        <Button teksti="huono" onClick={this.lisaaPalaute("huono")} />
         <Otsikko teksti="statistiikka" />
         <PalauteLista palautteet={this.state} />
-        <Statistiikka palautteet={this.state} />
+        <Statistics palautteet={this.state} />
       </div>
     )
   }
