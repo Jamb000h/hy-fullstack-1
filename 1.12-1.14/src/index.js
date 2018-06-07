5ea96e4bba6771e7199b414355ef62af436a60e5
Jonne Kanerva <jonne.kanerva@siili.com>
2018-06-07 13:41:42 +0300

    Tehtävä 1.13

--- 1.12-1.14/src/index.js
+++ 1.12-1.14/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Votes = props => {
+
+  const votes = props.votes[props.selected] ? props.votes[props.selected] : 0
+
+  return (
+    <p>
+      has {votes} votes
+    </p>
+  )
+}
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      selected: 0
+      selected: 0,
+      votes: {}
     }
   }
 
@@ -18,11 +29,25 @@ class App extends React.Component {
     }
   }
 
+  vote = () => {
+    return () => {
+      const votes = this.state.votes[this.state.selected] ? this.state.votes[this.state.selected] : 0
+      const kopio = {...this.state.votes}
+      kopio[this.state.selected] = votes + 1
+
+      this.setState({
+        votes: kopio
+      })
+    }
+  }
+
   render() {
     return (
       <div>
         {this.props.anecdotes[this.state.selected]}
+        <Votes votes={this.state.votes} selected={this.state.selected} />
         <br />
+        <button onClick={this.vote()}>vote</button>
         <button onClick={this.nextAnecdote()}>next anecdote</button>
       </div>
     )
