0ff1b574cb7bb9e4aa5074d9c1221d4e02b970eb
Jonne Kanerva <jonne.kanerva@siili.com>
2018-06-07 11:11:50 +0300

    Tehtävä 1.7

--- 1.6-1.11/src/index.js
+++ 1.6-1.11/src/index.js
@@ -21,6 +21,40 @@ const PalauteLista = props => {
 
 const Palaute = props => <p>{props.teksti} {props.arvo}</p>
 
+const Statistiikka = props => {
+  return (
+    <div>
+      <Keskiarvo palautteet={props.palautteet} />
+      <Positiivisia palautteet={props.palautteet} />
+    </div>
+  )
+}
+
+const Keskiarvo = props => {
+
+  const palautteita = Object.values(props.palautteet).reduce( (yht, palaute) => yht + palaute, 0)
+  const hyvatPisteet = props.palautteet.hyva * 1
+  const neutraalitPisteet = props.palautteet.neutraali * 0 // heh
+  const huonotPisteet = props.palautteet.huono * -1
+
+  const summa = hyvatPisteet + neutraalitPisteet + huonotPisteet
+
+  const keskiarvo = summa / palautteita
+
+  return (
+    <p>keskiarvo {keskiarvo.toFixed(1)}</p>
+  )
+}
+
+const Positiivisia = props => {
+  const palautteita = Object.values(props.palautteet).reduce( (yht, palaute) => yht + palaute, 0)
+  const positiivisia = props.palautteet.hyva / palautteita * 100
+
+  return (
+    <p>positiivisia {positiivisia.toFixed(1)} %</p>
+  )
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -48,6 +82,7 @@ class App extends React.Component {
         <button onClick={this.lisaaPalaute("huono")}>huono</button>
         <Otsikko teksti="statistiikka" />
         <PalauteLista palautteet={this.state} />
+        <Statistiikka palautteet={this.state} />
       </div>
     )
   }
